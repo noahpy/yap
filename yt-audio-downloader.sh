497ae41b38191d58893633292ac7737aadf6d4cc
noahpy <noah.schlenker2002@gmail.com>
2024-04-04 02:59:47 +0200

    Smol adjustments

--- yt-audio-downloader.sh
+++ yt-audio-downloader.sh
@@ -13,7 +13,7 @@ invidious=https://yt.artemislena.eu
 # temporary files path
 tmp='/tmp/yt-download'
 
-# itag for lowest quality m4a (https://gist.github.com/sidneys/7095afe4da4ae58694d128b1034e01e2)
+# itag for lowest quality m4a (see https://gist.github.com/sidneys/7095afe4da4ae58694d128b1034e01e2)
 itag=139
 
 mkdir -p "$tmp" finish
@@ -26,7 +26,7 @@ do
   link=$(echo $line | awk -F' ' '{print $1}')
 
   # forward link to ensure parameter is in link (also work with bit.ly links)
-  if [[ $link != *".com"* ]]; then
+  if [[ $link != *"youtube.com"* && $link != *"$invidious"* ]]; then
     link=$(echo $(curl -Ls -o /dev/null -w %{url_effective} $link))
   fi
 
@@ -47,29 +47,31 @@ do
   # Skip loop element, when file already exists
   [ -f finish/"$name".m4a ] && echo -e "Skipping \"$name\", because it already exists" && continue
 
-  echo -e "name=$name"
-
-  # get audio .m4a
-  echo "$invidious/latest_version?id=$id&itag=$itag"
+  echo "Curling $name from: $invidious/latest_version?id=$id&itag=$itag"
   curl -Ls "$invidious/latest_version?id=$id&itag=$itag" -o "$tmp/$name.m4a"
   RC=$(echo $?)
-  echo -e "curl audio: $RC" 
+  if [ $RC -ne 0 ]; then
+      echo "Could not curl audio!"
+  fi
 
   # get cover image
   [ $RC -eq 0 ] && curl -Ls "$invidious/vi/$id/maxres.jpg" -o "$tmp/$name-cover.jpg"
   RC=$(echo $?)
-  echo -e "curl picture: $RC" 
+  if [ $RC -ne 0 ]; then
+      echo "Could not curl thumbnail!"
+  fi
 
   # insert image to audio
   [ $RC -eq 0 ] && (ffmpeg -nostdin -i "$tmp/$name.m4a" -i "$tmp/$name-cover.jpg" -map 0:0 -map 1:0 -acodec copy -id3v2_version 3 "finish/$name.m4a") 2> /dev/null
-  echo -e "ffmpeg put image in m4a: $?"
-
+  if [ $RC -ne 0 ]; then
+      echo "Could add thumbnail to audio!"
+      cp "$tmp/$name.m4a" "finish/$name.m4a"
+  fi
 
   # remove temporary files
   [ -f "$tmp/$name-cover.jpg" ] && rm "$tmp/$name-cover.jpg"
   [ -f "$tmp/$name.m4a" ] && rm "$tmp/$name.m4a"
 
-  echo -e "rm temporary files: $?" 
 done < $list
 
 [ -f "$tmp" ] && rmdir "$tmp"
